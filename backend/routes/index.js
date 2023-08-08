02354a28cc568d716999fc66b0adbda0e4df3005
unknown <hamayoon.47611@iqra.edu.pk>
2023-08-08 12:34:14 +0500

    added router logout endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import controller from "../controller/authController.js";
+import auth from "../middleWare/auth.js";
 
 const router = express.Router();
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
-
+router.post("/logout", auth, controller.logout);
 export default router;
